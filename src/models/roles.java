cefe588eaeeaab9185e6c7494fcb7b4aea55e3ca
Seac2600 <canedoadam3@gmail.com>
2026-03-30 18:45:12 -0500

    got everything done i think

--- src/models/roles.java
+++ src/models/roles.java
@@ -1,5 +1,33 @@
 package models;
 
 public class roles {
+    private int id;
+    private static String[] roleNames; 
+
     
+
+    public roles() {
+        roleNames = new String[]{"Admin", "Dentist", "Receptionist", "patient"};
+    }
+
+    public roles(int id, String name) {
+        this.id = id;
+        roleNames = new String[]{"Admin", "Dentist", "Receptionist", "patient"};
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return roleNames[id];
+    }
+
+    public void setRoleName(String name) {
+        this.roleNames[id] = name;
+    }
 }
--- src/services/adminMangment.java
+++ src/services/adminMangment.java
@@ -1,7 +1,46 @@
 package services;
+import models.User;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class adminMangment {
-    public static void main(String[] args) 
+    private List<User> users = new ArrayList<>();
+    
+    public void addUser(User user)
     {
+        users.add(user);
+    }
+    public boolean deleteUser(int Id) {
+       try {
+            if (users.removeIf(user -> user.getId() == Id)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    
+    }
+    public boolean editUser(User user) {
+        try{
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getId() == user.getId()) {
+                    users.set(i, user);
+                    return true;
+                }
+            }
+             return false;
+        } catch (Exception e) {
+            return false;
+        }
+     
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 }
